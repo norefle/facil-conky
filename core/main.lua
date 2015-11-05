caa85e4ca34e467d958af001e43bb0aef7e991d0
Dim L. Feiron <norefle@outlook.com>
2015-11-05 11:43:12 +0100

    Title colors depedns on wip.

--- core/main.lua
+++ core/main.lua
@@ -1,7 +1,7 @@
 require "cairo"
 
 local Facil = require "facil"
-local Moses = require "moses"
+local M = require "moses"
 local Theme = nil
 
 local Conf = {}
@@ -38,6 +38,26 @@ Conf.Tasks.Limit = 10
     @endcode
 ]]--
 
+local function boardAsString(board)
+    return string.format("[ %3d | %3d ] %s",
+        #board.tasks,
+        board.wip,
+        board.name
+    )
+end
+
+local function taskAsString(task)
+    return string.format("%s %s (%s)",
+        os.date("%d.%m.%Y", task.moved),
+        task.name,
+        task.id:sub(1, 8)
+    )
+end
+
+local function exceededWip(board)
+    return (board.wip ~= 0) and (#board.tasks >= board.wip)
+end
+
 function conky_main(style, path)
     assert(style, "Expects not null style name")
     assert(path, "Expects not null path to facil")
@@ -61,32 +81,32 @@ function conky_main(style, path)
     )
 
     local display = cairo_create(surface)
-    cairo_set_source_rgba(display, 0, 0, 255, 1)
 
     cairo_select_font_face(display, "Droid Sans Mono Slashed", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL)
     cairo_set_font_size(display, 12)
 
     local states, description = Facil.status(path)
     if states then
-        -- Transfrom to string list.
-        local todoList = Moses.flatten(
-            Moses.map(states, function(_, board)
-                local title = string.format("[ %3d | %3d ] %s", #board.tasks, board.wip, board.name)
-                local tasks = Moses.map(board.tasks, function(_, task)
-                    return string.format("%s %s (%s)",
-                        os.date("%d.%m.%Y", task.moved),
-                        task.name,
-                        task.id:sub(1, 8)
-                    )
-                end)
-                return Moses.addTop(Moses.take(tasks, (board.limit ~= 0) and board.limit or Conf.Tasks.Limit), { "", title })
-            end)
+        -- Transfrom to list of tuples: (string, color).
+        local todoList = M.flatten(
+            M.map(states, function(_, board)
+                local titleColor = exceededWip(board) and Theme.Color.Warning or Theme.Color.Success
+                local textColor = Theme.Color.Text
+
+                local title = { boardAsString(board), titleColor }
+
+                local tasks = M.map(board.tasks, function(_, task) return { taskAsString(task), textColor } end)
+                local tasksPrefix = M.take(tasks, (board.limit ~= 0) and board.limit or Conf.Tasks.Limit)
+                return M.addTop(tasksPrefix, title, { "", Theme.Color.Text })
+            end),
+            true
         )
         -- Print result on desktop.
-        Moses.forEach(todoList, function(index, value)
-            cairo_set_source_rgba(display, unpack(Theme.Color.Text))
+        M.forEach(todoList, function(index, value)
+            local text, color = unpack(value)
+            cairo_set_source_rgba(display, unpack(color))
             cairo_move_to(display, Conf.Tasks.Left, Conf.Tasks.Top  + 20 * index)
-            cairo_show_text(display, value)
+            cairo_show_text(display, text)
         end)
     end
 
